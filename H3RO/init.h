e5289a8d0c15ff42556323e543988e83c061e65b
LouisFontaine <louis.fontaine@efrei.net>
2016-11-07 14:28:52 +0100

    Delete init.h

--- H3RO/init.h
+++
@@ -1,8 +0,0 @@
-#include "struct.h"
-
-/* Prototypes des fonctions utilisées */
-extern SDL_Surface *loadImage(char *name);
-extern void loadMap(char *name);
-
-extern Operate jeu;
-extern Depth Map;
